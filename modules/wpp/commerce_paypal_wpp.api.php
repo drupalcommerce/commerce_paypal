c1e924c31a36773098df90ba1fae4425355cd013
mikehues <mikehues@464176.no-reply.drupal.org>
2012-02-04 15:14:38 +0100

    Issue #1408244 by mikehues: allow an order object to be passed in with a PayPal WPP request that makes it through to the API request's alter function.

--- modules/wpp/commerce_paypal_wpp.api.php
+++ modules/wpp/commerce_paypal_wpp.api.php
@@ -12,7 +12,10 @@
  *
  * @param &$nvp
  *   The name-value pair array for the API request.
+ * @param $order
+ *   If available, the full order object the payment request is being submitted
+ *   for; otherwise NULL.
  */
-function hook_commerce_paypal_wpp_request_alter(&$nvp) {
+function hook_commerce_paypal_wpp_request_alter(&$nvp, $order) {
   // No example.
 }
