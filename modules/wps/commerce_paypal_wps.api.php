76651d10a4950020357e770fe84d46aaa02bdf61
rszrama <ryan.szrama@gmail.com>
2013-01-04 00:23:34 -0500

    Issue #1864842 by rszrama: abstract the SetExpressCheckout API request out of form submit handlers to its own function and use that for the EC and Mark flow of Express Checkout.

--- modules/wps/commerce_paypal_wps.api.php
+++ modules/wps/commerce_paypal_wps.api.php
@@ -14,6 +14,8 @@
  *   The data array used to create redirect form elements.
  * @param $order
  *   The full order object the redirect form is being generated for.
+ *
+ * @see commerce_paypal_wps_order_form()
  */
 function hook_commerce_paypal_wps_order_form_data_alter(&$data, $order) {
   // No example.
