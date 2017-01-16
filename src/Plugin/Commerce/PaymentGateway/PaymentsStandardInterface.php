c5a4e4b24b8f29f0dc39bdb5a87d8168d3fefd97
mglaman <mglaman@2416470.no-reply.drupal.org>
2017-01-16 11:50:20 -0600

    Issue #2843995 by mglaman: Implement Payments Standard

---
+++ src/Plugin/Commerce/PaymentGateway/PaymentsStandardInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\commerce_paypal\Plugin\Commerce\PaymentGateway;
+
+use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsiteRedirectPaymentGatewayInterface;
+
+interface PaymentsStandardInterface extends OffsiteRedirectPaymentGatewayInterface {
+
+}
