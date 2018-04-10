f0dddfd3c540bca7b4e85bae47d73fcdf3b34ac5
steveoliver <steveoliver@42327.no-reply.drupal.org>
2018-04-10 11:22:45 -0500

    Issue #2949233 by steveoliver: Save authorization code with transaction number in Remote id field

--- src/Plugin/Commerce/PaymentGateway/Payflow.php
+++ src/Plugin/Commerce/PaymentGateway/Payflow.php
@@ -228,6 +228,51 @@ class Payflow extends OnsitePaymentGatewayBase implements PayflowInterface {
   }
 
   /**
+   * Get the remote transaction number ('pnref') of a payment.
+   *
+   * @param \Drupal\commerce_payment\Entity\PaymentInterface $payment
+   *   The payment.
+   *
+   * @return string
+   *   The Payflow transaction number.
+   */
+  protected function getTransactionNumber(PaymentInterface $payment) {
+    return explode('|', $payment->getRemoteId())[0];
+  }
+
+  /**
+   * Get the remote authorization code ('authcode') of a payment.
+   *
+   * @param \Drupal\commerce_payment\Entity\PaymentInterface $payment
+   *   The payment.
+   *
+   * @return string
+   *   The Payflow authorization code.
+   */
+  protected function getAuthorizationCode(PaymentInterface $payment) {
+    $remote_id = $payment->getRemoteId();
+
+    return (strpos($remote_id, '|') !== FALSE) ? explode('|', $remote_id)[1] : $remote_id;
+  }
+
+  /**
+   * Gets a composite Remote ID from two Payflow payment transaction fields.
+   *
+   * @param array $data
+   *   A data array including 'pnref' and 'authcode' keys.
+   *
+   * @return bool|string
+   *   FALSE if required keys are missing, or a string "<pnref>|<authcode>".
+   */
+  protected function prepareRemoteId(array $data) {
+    if (!array_key_exists('pnref', $data) || !array_key_exists('authcode', $data)) {
+      return FALSE;
+    }
+
+    return $data['pnref'] . '|' . $data['authcode'];
+  }
+
+  /**
    * Prepares the request body to name/value pairs.
    *
    * @param array $parameters
@@ -352,7 +397,7 @@ class Payflow extends OnsitePaymentGatewayBase implements PayflowInterface {
       }
 
       $payment
-        ->setRemoteId($data['pnref'])
+        ->setRemoteId($this->prepareRemoteId($data))
         ->setRemoteState('3')
         ->save();
     }
@@ -375,7 +420,7 @@ class Payflow extends OnsitePaymentGatewayBase implements PayflowInterface {
         'trxtype' => 'D',
         'amt' => $this->rounder->round($amount)->getNumber(),
         'currency' => $amount->getCurrencyCode(),
-        'origid' => $payment->getRemoteId(),
+        'origid' => $this->getTransactionNumber($payment),
       ]);
 
       if ($data['result'] !== '0') {
@@ -397,7 +442,7 @@ class Payflow extends OnsitePaymentGatewayBase implements PayflowInterface {
   public function voidPayment(PaymentInterface $payment) {
     $this->validatePayment($payment, 'authorization');
 
-    $remoteId = $payment->getRemoteId();
+    $remoteId = $this->getTransactionNumber($payment);
 
     if (empty($remoteId)) {
       throw new PaymentGatewayException('Remote authorization ID could not be determined.');
@@ -406,7 +451,7 @@ class Payflow extends OnsitePaymentGatewayBase implements PayflowInterface {
     try {
       $data = $this->executeTransaction([
         'trxtype' => 'V',
-        'origid' => $payment->getRemoteId(),
+        'origid' => $this->getTransactionNumber($payment),
         'verbosity' => 'HIGH',
       ]);
 
@@ -436,7 +481,8 @@ class Payflow extends OnsitePaymentGatewayBase implements PayflowInterface {
     $amount = $amount ?: $payment->getAmount();
     $amount = $this->rounder->round($amount);
     $this->assertRefundAmount($payment, $amount);
-    if (empty($payment->getRemoteId())) {
+    $transaction_number = $this->getTransactionNumber($payment);
+    if (empty($transaction_number)) {
       throw new InvalidRequestException('Could not determine the remote payment details.');
     }
 
@@ -445,7 +491,7 @@ class Payflow extends OnsitePaymentGatewayBase implements PayflowInterface {
 
       $data = $this->executeTransaction([
         'trxtype' => 'C',
-        'origid' => $payment->getRemoteId(),
+        'origid' => $transaction_number,
       ]);
       if ($data['result'] !== '0') {
         throw new PaymentGatewayException('Credit could not be completed. Message: ' . $data['respmsg'], $data['result']);
